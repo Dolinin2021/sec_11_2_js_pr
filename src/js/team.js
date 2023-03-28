d3788c50e076c842e4884e556bd46020a2e37be0
Dolinin2021 <programmistdolinin@mail.ru>
2023-03-28 15:56:45 +0600

    fix

--- src/js/team.js
+++ src/js/team.js
@@ -3,18 +3,7 @@ export default class Team {
     this.arr = arr;
   }
 
-  [Symbol.iterator]() {
-    const { arr } = this;
-    let nextIndex = 0;
-    return {
-      next() {
-        if (nextIndex < arr.length) {
-          const result = { value: arr[nextIndex], done: false };
-          nextIndex += 1;
-          return result;
-        }
-        return { done: true };
-      },
-    };
+  * [Symbol.iterator]() {
+    yield* this.arr;
   }
 }
